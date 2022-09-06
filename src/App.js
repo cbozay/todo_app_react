fac818035d7463d3226b04b7228e709466fd9a59
cbozay <cbozay90@hotmail.com>
2022-09-06 13:47:25 +0300

    add todo and isDone

--- src/App.js
+++ src/App.js
@@ -2,6 +2,17 @@ import React, { useState } from "react";
 
 function App() {
   const [todoText, setTodoText] = useState("");
+  const [todos, setTodos] = useState([]);
+
+  const changeIsDone = (id) => {
+    const searchedTodo = todos.find((item) => item.id === id);
+    const updatedTodo = {
+      ...searchedTodo,
+      isDone: !searchedTodo.isDone,
+    };
+    const filteredTodos = todos.filter((item) => item.id !== id);
+    setTodos([updatedTodo, ...filteredTodos]);
+  };
 
   const handleSubmit = (event) => {
     event.preventDefault();
@@ -9,7 +20,22 @@ function App() {
       alert("todoText cant be empty");
       return;
     }
-    console.log(todoText);
+    const hasTodos = todos.find((item) => item.text === todoText);
+    console.log(hasTodos);
+    if (hasTodos !== undefined) {
+      alert("You have the todo already");
+      return;
+    }
+
+    const newTodo = {
+      id: new Date().getTime(),
+      isDone: false,
+      text: todoText,
+      date: new Date(),
+    };
+    setTodos([newTodo, ...todos]);
+    setTodoText("");
+    console.log(newTodo);
   };
   return (
     <div className="container">
@@ -21,15 +47,35 @@ function App() {
             type="text"
             className="form-control"
             placeholder="Type your Todo"
-            onChange={(event) => {
-              setTodoText(event.target.value);
-            }}
+            onChange={(event) => setTodoText(event.target.value)}
           />
           <button className="btn btn-primary" type="submit">
             ADD
           </button>
         </div>
       </form>
+      {todos.length <= 0 ? (
+        <p className="text-center my-5">You don't have any todos.</p>
+      ) : (
+        <>
+          {todos.map((item) => (
+            <div
+              className="alert alert-secondary d-flex justify-content-between"
+              role="alert"
+            >
+              <p className="my-auto ">{item.text}</p>
+              <button
+                onClick={() => {
+                  changeIsDone(item.id);
+                }}
+                className="btn btn-sm btn-secondary"
+              >
+                {item.isDone === false ? "Done" : "Undone"}
+              </button>
+            </div>
+          ))}
+        </>
+      )}
     </div>
   );
 }
